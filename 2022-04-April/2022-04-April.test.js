4d838e999789ebfbfab58d816484d30b80d3bc30
Caleb Weeks <cweeks@imsdirect.com>
2022-04-19 16:09:56 -0400

    Cleared out April tests

--- 2022-04-April/2022-04-April.test.js
+++ 2022-04-April/2022-04-April.test.js
@@ -1,59 +1,7 @@
-const {
-  uniqueDigits,
-  sudokuRowChecker,
-  sudokuChecker,
-} = require("./2022-04-April");
+const { missingDigits, validDigits, sudokuSolver } = require("./2022-04-April");
 
-const validRow = [3, 2, 1, 6, 5, 4, 9, 8, 7];
-const invalidRow = [3, 2, 1, 6, 5, 4, 9, 8, 1];
+describe("[Easy]", () => {});
 
-const validSolution = [
-  [3, 2, 1, 6, 5, 4, 9, 8, 7],
-  [6, 5, 4, 9, 8, 7, 3, 2, 1],
-  [9, 8, 7, 3, 2, 1, 6, 5, 4],
-  [2, 1, 3, 5, 4, 6, 8, 7, 9],
-  [5, 4, 6, 8, 7, 9, 2, 1, 3],
-  [8, 7, 9, 2, 1, 3, 5, 4, 6],
-  [1, 3, 2, 4, 6, 5, 7, 9, 8],
-  [4, 6, 5, 7, 9, 8, 1, 3, 2],
-  [7, 9, 8, 1, 3, 2, 4, 6, 5],
-];
+describe("[Medium]", () => {});
 
-const invalidSolution = [
-  [3, 2, 1, 6, 5, 4, 9, 8, 7],
-  [6, 5, 4, 9, 8, 7, 3, 2, 1],
-  [9, 8, 7, 3, 2, 1, 6, 5, 4],
-  [2, 1, 3, 5, 4, 6, 8, 7, 9],
-  [5, 4, 6, 8, 1, 9, 2, 1, 3],
-  [8, 7, 9, 2, 1, 3, 5, 4, 6],
-  [1, 3, 2, 4, 6, 5, 7, 9, 8],
-  [4, 6, 5, 7, 9, 8, 1, 3, 2],
-  [7, 9, 8, 1, 3, 2, 4, 6, 5],
-];
-
-describe("[Easy]", () => {
-  test("Returns true for a valid row", () => {
-    expect(uniqueDigits(validRow)).toBe(true);
-  });
-  test("Returns false for an invalid row", () => {
-    expect(uniqueDigits(invalidRow)).toBe(false);
-  });
-});
-
-describe("[Medium]", () => {
-  test("Returns true for a valid solution", () => {
-    expect(uniqueDigits(validSolution)).toBe(true);
-  });
-  test("Returns false for an invalid solution", () => {
-    expect(uniqueDigits(invalidSolution)).toBe(false);
-  });
-});
-
-describe("[Hard]", () => {
-  test("Returns true for a valid solution", () => {
-    expect(uniqueDigits(validSolution)).toBe(true);
-  });
-  test("Returns false for an invalid solution", () => {
-    expect(uniqueDigits(invalidSolution)).toBe(false);
-  });
-});
+describe("[Hard]", () => {});
